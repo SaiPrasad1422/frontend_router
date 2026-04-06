60afc6ef2e38fe905c20f053379731b3d6896812
SaiPrasad1422 <katkoori.saiprasadrao@gmail.com>
2026-04-06 22:50:43 +0530

    rl_table

--- network-dashboard/src/components/RulesTable.jsx
+++ network-dashboard/src/components/RulesTable.jsx
@@ -7,29 +7,52 @@ function RulesTable({ data, onDelete }) {
           <th>Device IP</th>
           <th>Remote IP</th>
           <th>Action</th>
+          <th>Transport</th> {/* NEW */}
           <th>Protocol</th>
           <th>Direction</th>
+          <th>Rate</th> {/* NEW */}
+          <th>Priority</th> {/* NEW */}
+          <th>Note</th> {/* NEW */}
+          <th>Created At</th> {/* NEW */}
           <th>Remove</th>
         </tr>
       </thead>
 
       <tbody>
-        {data.map((rule) => (
-          <tr key={rule.id}>
-            <td>{rule.id}</td>
-            <td>{rule.device}</td>
-            <td>{rule.remote}</td>
-            <td>{rule.action}</td>
-            <td>{rule.protocol}</td>
-            <td>{rule.direction}</td>
+        {Array.isArray(data) && data.length > 0 ? (
+          data.map((rule) => (
+            <tr key={rule.id}>
+              <td>{rule.id}</td>
+              <td>{rule.device}</td>
+              <td>{rule.remote}</td>
+              <td>{rule.action}</td>
 
-            <td>
-              <button onClick={() => onDelete(rule.id)}>
-                Remove
-              </button>
-            </td>
+              {/* 🔥 NEW FIELDS */}
+              <td>{rule.transport}</td>
+              <td>{rule.protocol}</td>
+              <td>{rule.direction}</td>
+              <td>{rule.rate || "-"}</td>
+              <td>{rule.priority || "-"}</td>
+              <td>{rule.note || "-"}</td>
+
+              <td>
+                {rule.created_at
+                  ? new Date(rule.created_at).toLocaleString()
+                  : "-"}
+              </td>
+
+              <td>
+                <button onClick={() => onDelete(rule.id)}>
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan="12">No rules found</td>
           </tr>
-        ))}
+        )}
       </tbody>
     </table>
   );
